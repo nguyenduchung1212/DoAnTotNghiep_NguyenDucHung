3d8ae1af6fc53c8947c7bd4060d4e62c312833aa
Duc Hung <nguyenduchung1212@gmail.com>
2022-07-02 21:20:06 +0700

    update mail order

--- resources/views/mail/mail_confirm_order.blade.php
+++ resources/views/mail/mail_confirm_order.blade.php
@@ -1,5 +1,7 @@
+<?php $url = 'search-order?code_invoice=' ;
+?>
 <h2>Gửi {{$name}},</h2>
 <p>Đơn hàng của bạn đã được tiếp nhận</p>
 <p>Mã đơn hàng của bạn là {{$code}} </p>
+<p>Nhấn vào link để xem tình trạng đơn hàng {{Config::get('app.url').$url.$code}} </p>
 <p>Cảm ơn bạn đã tin tưởng </p>
-
